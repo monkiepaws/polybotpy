cd76b7a536bcbb9638782904a7dec97eb92626de
Richard Sims <sims.richo@gmail.com>
2021-06-05 15:22:06 +1000

    added commands to setup patching

--- ops/setup.sh
+++ ops/setup.sh
@@ -2,7 +2,10 @@
 # 2. Assumes a user called polybot
 
 POLYBOT_SERVICE_NAME="polybot_discord.service"
+POLYBOT_PATCH_NAME="polybot_patch.timer"
 
 sudo systemctl daemon-reload
 sudo systemctl start $POLYBOT_SERVICE_NAME
 sudo systemctl enable $POLYBOT_SERVICE_NAME
+sudo systemctl start $POLYBOT_PATCH_NAME
+sudo systemctl enable $POLYBOT_PATCH_NAME
